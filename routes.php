79be4df83b8a28725419609a4889707c875a0fec
Artūrs Krusts <85063833+Arturs322@users.noreply.github.com>
2022-11-08 23:22:53 +0200

    deleted unnecessary file

--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
-
-get('/', 'index.php');
-get('/list', 'public/test.php');
-get('/addProduct', 'public/addProduct.php');
-
-
-any('/404','views/404.php');
