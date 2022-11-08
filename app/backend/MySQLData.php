d4a4b7bb142e3fb7b1ff339da5f0604f2b5ae07a
Artūrs Krusts <85063833+Arturs322@users.noreply.github.com>
2022-11-08 23:28:27 +0200

    removed unnecessary conditional statements

--- app/backend/MySQLData.php
+++ app/backend/MySQLData.php
@@ -6,17 +6,16 @@ class MySQLData extends DatabaseConnection implements QueryInterface {
 
     public function read()
     {
-        $conn = $this->getMySQLConnection();
-        return mysqli_query($conn, "SELECT * FROM " . $this->dbtable . " ORDER BY id");
+        return mysqli_query($this->getMySQLConnection(), "SELECT * FROM " . $this->dbtable . " ORDER BY id");
     }
 
     public function delete()
     {
-        $conn = $this->getMySQLConnection();
+        
         if (isset($_POST['delete-product-btn'])) {
             $arr = $_POST['checkbox'];
             foreach ($arr as $id) {
-                mysqli_query($conn, "DELETE FROM " . $this->dbtable . " WHERE id =" . $id);
+                mysqli_query($this->getMySQLConnection(), "DELETE FROM " . $this->dbtable . " WHERE id =" . $id);
             }
 
         }
@@ -24,9 +23,7 @@ class MySQLData extends DatabaseConnection implements QueryInterface {
 
     public function insert($SKU, $name, $price, $value, $choose)
     {
-
-        $conn = $this->getMySQLConnection();
-        mysqli_query($conn, "INSERT INTO " . $this->dbtable . "(SKU, name, price, {$choose}) VALUES 
+        mysqli_query($this->getMySQLConnection(), "INSERT INTO " . $this->dbtable . "(SKU, name, price, {$choose}) VALUES 
         ('$SKU', '$name', '$price', '$value')") or die(mysqli_error($conn));
 
     }
